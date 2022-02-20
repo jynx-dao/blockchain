fb921df423ab6fa76c62b6f5ee75beb347da70dc
First Excited State <firstexcitedstate@protonmail.com>
2022-02-20 10:51:23 +0000

    assert unrealised pnl

--- src/test/java/com/jynx/pro/service/OrderServiceTest.java
+++ src/test/java/com/jynx/pro/service/OrderServiceTest.java
@@ -389,6 +389,13 @@ public class OrderServiceTest extends IntegrationTest {
                 takerMarginBalance.setScale(dps, RoundingMode.HALF_UP));
         Assertions.assertEquals(takerAccountOptional.get().getBalance().setScale(dps, RoundingMode.HALF_UP),
                 takerStartingBalance.setScale(dps, RoundingMode.HALF_UP));
-        // TODO - unrealised PNL
+        BigDecimal gain = market.getLastPrice().subtract(price).divide(price, dps, RoundingMode.HALF_UP);
+        // TODO - check this, I think it's asserting zero because it currently uses last price (when it should use mid price)
+        BigDecimal makerUnrealisedProfit = gain.multiply(size).multiply(price).abs();
+        BigDecimal takerUnrealisedProfit = makerUnrealisedProfit.multiply(BigDecimal.valueOf(-1));
+        Assertions.assertEquals(makerUnrealisedProfit.setScale(dps, RoundingMode.HALF_UP),
+                positionMaker.getUnrealisedPnl().setScale(dps, RoundingMode.HALF_UP));
+        Assertions.assertEquals(takerUnrealisedProfit.setScale(dps, RoundingMode.HALF_UP),
+                positionTaker.getUnrealisedPnl().setScale(dps, RoundingMode.HALF_UP));
     }
 }
