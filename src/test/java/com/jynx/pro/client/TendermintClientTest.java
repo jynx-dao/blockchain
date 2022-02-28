ea390e45d59c478a829513600ac5f81d5471f857
First Excited State <firstexcitedstate@protonmail.com>
2022-02-28 10:35:26 +0000

    integration tests with tendermint

--- src/test/java/com/jynx/pro/client/TendermintClientTest.java
+++ src/test/java/com/jynx/pro/client/TendermintClientTest.java
@@ -3,6 +3,7 @@ package com.jynx.pro.client;
 import com.jynx.pro.Application;
 import com.jynx.pro.blockchain.TendermintClient;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +16,7 @@ import org.testcontainers.utility.DockerImageName;
 @Slf4j
 @Testcontainers
 @ActiveProfiles("tendermint")
-@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TendermintClientTest {
 
     @Autowired
@@ -25,15 +26,10 @@ public class TendermintClientTest {
     public static GenericContainer tendermint =
             new GenericContainer(DockerImageName.parse("tendermint/tendermint:v0.33.8"))
             .withCommand("node --abci grpc --proxy_app tcp://host.docker.internal:26658")
-            .withExtraHost("host.docker.internal", "host-gateway")
-            .withAccessToHost(true);
-
-    static {
-        org.testcontainers.Testcontainers.exposeHostPorts(26658, 26657, 26656, 26660);
-    }
+            .withExtraHost("host.docker.internal", "host-gateway");
 
     @Test
     public void test() throws InterruptedException {
-        Thread.sleep(20000L);
+        Thread.sleep(5000L);
     }
 }
